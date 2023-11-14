d3993ee2f4840301a1995e114fdd0b39c14c5a47
pluzinc <132268195+pluzinc@users.noreply.github.com>
2023-11-14 01:10:27 -0500

    had my notification functions running

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ require('dotenv').config()
 var cron = require('node-cron')
 var Pushover = require('node-pushover')
 
-// cron.schedule('*/5 * * * * *', notify)
+cron.schedule('*/5 * * * * *', notify);
 
-cron.schedule(' * 9 * * *', callback)
+cron.schedule('* 9 * * *', callback);
 
-cron.schedule(' 10 10 * * *', appointment)
+cron.schedule('55 14 * * *', appointment);
 
-cron.schedule(' * */2 * * *', exercise)
+cron.schedule('5 1 * * *', appointment);
+
+cron.schedule('*/1 * * * *', exercise);
 
 var push = new Pushover({
     token: process.env.APP_TOKEN,
@@ -17,18 +19,26 @@ var push = new Pushover({
 })
 
 function notify() {
-    push.send("homework testing", "My message", handleErrors)
+    push.send("is pm2 working?", "My message", handleErrors)
+    log("Running notify now");
 }
 function callback() {
     push.send("good morning", "get some coffee. do u belive in luck? 🍀", handleErrors)
+    log("Running callback now");
 }
-function callback() {
+function appointment() {
     push.send("Doctor's Appointment", "Be well~", handleErrors)
+    log("Running appointment now");
 }
-function callback() {
+function exercise() {
     push.send("Break Time", "Stand up and take a walk around!", handleErrors)
+    log("Running exercise now");
 }
 function handleErrors(error, response) {
     if (error) console.log('error: ', error)
     console.log(response)
+}
+
+function log(msg) {
+    console.log(msg, new Date().toLocaleTimeString());
 }
